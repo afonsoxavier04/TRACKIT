514006cce7ee481470a73d8967216311fad04fc4
afonsoxavier04 <35966@esjaloures.org>
2023-02-13 18:45:53 +0000

    base de dados/ conexao com php

--- databse/trackit-tables.sql
+++ databse/trackit-tables.sql
@@ -6,6 +6,9 @@ CREATE TABLE logins(
     id INT(11) UNIQUE UNSIGNED NOT NULL,
     email VARCHAR(70) NOT NULL,
     password VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    update_at TIMESTAMP,
+    deleted_at TIMESTAMP
 )engine = InnoDB;
 
 CREATE TABLE users(
@@ -15,16 +18,16 @@ CREATE TABLE users(
     password VARCHAR(50 NOT NULL,
     id_package INT(11) UNIQUE NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    deleted_at TIMESTAMP NOT NULL
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
 )engine = InnoDB;
 
 CREATE TABLE package(
     id INT(11) UNIQUE UNSIGNED NOT NULL,
     receiver_adress VARCHAR(100) NOT NULL,
     user_id INT(11) UNIQUE NOT NULL,
-    received_date TIMESTAMP NOT NULL,
-    sent_date TIMESTAMP NOT NULL,
+    received_date TIMESTAMP,
+    sent_date TIMESTAMP,
 
 )engine = InnoDB;
 
--- forms/signin.php
+++ forms/signin.php
@@ -12,5 +12,21 @@
     $logmail = $_POST("logemail");
     $logpass = $_POST("logpass");
 
+
+//          DATABASE CONNECTION
+
+    $con = mysqli_connect("localhost","root","","trackit");
+
+    $stmt = mysqli_prepare($con, "INSERT INTO logins(email,password) VALUES (?, ?, ?)");
+    mysqli_stmt_bind_param($stmt, $logmail, $logpass);
+
+    if (mysqli_stmt_execute($stmt)) {
+        echo "Record inserted successfully";
+    } else {
+        echo "Error: " . mysqli_error($con);
+    }
+
+    mysqli_stmt_close($stmt);
+    mysqli_close($con);
     
 ?>
