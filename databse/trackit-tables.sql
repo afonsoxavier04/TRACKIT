a84dce350e761fd7643345ff8a4f53a87ac75d65
afonsoxavier04 <35966@esjaloures.org>
2023-02-13 18:20:54 +0000

    ficheiros

---
+++ databse/trackit-tables.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS trackit;
+CREATE DATABASE trackit CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE trackit;
+
+CREATE TABLE logins(
+    id INT(11) UNIQUE UNSIGNED NOT NULL,
+    email VARCHAR(70) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+)engine = InnoDB;
+
+CREATE TABLE users(
+    id INT(11) UNIQUE UNSIGNED NOT NULL,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNSIGNED UNIQUE NOT NULL,
+    password VARCHAR(50 NOT NULL,
+    id_package INT(11) UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMP NOT NULL
+)engine = InnoDB;
+
+CREATE TABLE package(
+    id INT(11) UNIQUE UNSIGNED NOT NULL,
+    receiver_adress VARCHAR(100) NOT NULL,
+    user_id INT(11) UNIQUE NOT NULL,
+    received_date TIMESTAMP NOT NULL,
+    sent_date TIMESTAMP NOT NULL,
+
+)engine = InnoDB;
+
---
+++ forms/signin.php
@@ -0,0 +1,16 @@
+<?php
+//          LOGIN
+
+    $signmail = $_POST("logemail");
+    $signpass = $_POST("logpass");
+
+
+
+//          REGISTER
+
+    $logname = $_POST("logname");
+    $logmail = $_POST("logemail");
+    $logpass = $_POST("logpass");
+
+    
+?>
