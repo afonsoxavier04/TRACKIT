91792772c29d44cdd51b542e08c8829c62f66570
colhoes1337 <floppabis@gmail.com>
2023-02-14 22:26:01 +0000

    bwe da cenas v2

--- forms/signin.php
+++ forms/signin.php
@@ -39,36 +39,3 @@
         mysqli_close($con);
     }
 ?>
-
-
-
-Ledge
-podes traduzir esse codigo para ingles
-Certainly! Here's the translation of the code:
-
-php
-Copy code
-<?php
-//          LOGIN
-
-    $signmail = $_POST["email"];
-    $signpass = $_POST["password"];
-
-//          DATABASE CONNECTION
-
-    require("../conect.php");
-
-    $stmt = mysqli_prepare($con, "SELECT * FROM logins");
-    mysqli_stmt_bind_param($stmt, $logname, $logmail, $logpass);
-
-    if (mysqli_stmt_execute($stmt)) {
-        echo "Record inserted successfully";
-        echo $stmt;
-    } else {
-        echo "Error: " . mysqli_error($con);
-    }
-
-    mysqli_stmt_close($stmt);
-    mysqli_close($con);
-    
-?>
