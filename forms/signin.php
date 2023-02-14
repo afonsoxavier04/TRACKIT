376bbc3445ac628d07e2b25a88257ce8ea64fe97
afonsoxavier04 <35966@esjaloures.org>
2023-02-14 10:12:58 +0000

    hooray

--- forms/signin.php
+++ forms/signin.php
@@ -15,13 +15,8 @@
 
     $con = mysqli_connect("localhost","root","","trackit");
 
-<<<<<<< HEAD
-    $stmt = mysqli_prepare($con, "INSERT INTO logins(email,password) VALUES (?, ?, ?)");
-    mysqli_stmt_bind_param($stmt, $logmail, $logpass);
-=======
     $stmt = mysqli_prepare($con, "INSERT INTO logins(nome,email,password) VALUES (?, ?, ?)");
     mysqli_stmt_bind_param($stmt, $logname, $logmail, $logpass);
->>>>>>> 9eeb3599bc7ca2c5091769a01ad932c60a8aec37
 
     if (mysqli_stmt_execute($stmt)) {
         echo "Record inserted successfully";
